df41553861f1152cf989163a96f5598f3706c707
윤형석 <104922729+pledge24@users.noreply.github.com>
2023-12-28 16:08:31 +0900

    First Commit
    vscode - github 연결 확인

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int X = 10;
+    cout << "test 1" << '\n';
+
+    cout << "test 2" << '\n';
+
+    cout << "test 3" << '\n';
+
+    X = 256;
+    cout << "test 4" << '\n';
+}
