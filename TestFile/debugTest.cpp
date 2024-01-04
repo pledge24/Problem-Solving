1933f92626ffbc6b39a5b0820329b307ba3cc6f5
pledge24 <104922729+pledge24@users.noreply.github.com>
2024-01-04 19:58:23 +0900

    윈도우 설정 완료
    왔다갔다 커밋하다가 한 쪽 설정 날라가면 나 울어

---
+++ TestFile/debugTest.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    int a = 4;
+
+    a=5;
+
+
+    a=-1;
+    
+    return 0;
+}
--- TestFile/기본템플릿.cpp
+++ TestFile/기본템플릿.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(void){
-
+    cout << "test" << '\n';
     return 0;
 }
