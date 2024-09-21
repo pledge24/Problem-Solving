5118385e6c789a0009a514ea303c2c8a174763ee
윤형석 <104922729+pledge24@users.noreply.github.com>
2024-09-21 16:58:02 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -1,49 +1,36 @@
 #include <bits/stdc++.h>
 
 #define fastio cin.tie(0)->sync_with_stdio(0)
+#define point pair<long long, long long>
 
 using namespace std;
 
-struct Point{
-    long long x;
-    long long y;
-
-    bool operator<(const Point& other) const {
-        return std::tie(x, y) < std::tie(other.x, other.y);
-    }
-};
-
-int ccw(Point A, Point B, Point C) { 
-    long long res = (B.x - A.x)*(C.y - A.y) - (C.x - A.x)*(B.y - A.y);
+// first => x pos
+// second => y pos
+int ccw(point A, point B, point C) { 
+    long long res = (B.first - A.first)*(C.second - A.second) - (C.first - A.first)*(B.second - A.second);
 
     return (res > 0) - (res < 0);
 }
 
-bool isIntersect(pair<Point, Point> l1, pair<Point, Point> l2) {
+bool isIntersect(pair<point, point> l1, pair<point, point> l2) {
 
-    Point p1 = l1.first;
-    Point p2 = l1.second;
-    Point p3 = l2.first;
-    Point p4 = l2.second;
+    point p1 = l1.first;
+    point p2 = l1.second;
+    point p3 = l2.first;
+    point p4 = l2.second;
 
     int p1p2 = ccw(p1, p2, p3) * ccw(p1, p2, p4); // l1 기준
     int p3p4 = ccw(p3, p4, p1) * ccw(p3, p4, p2); // l2 기준
 
-    //cout << p1p2 << " " << p3p4 << '\n';
-
     // 두 직선이 일직선 상에 존재
     if (p1p2 == 0 && p3p4 == 0) {
 
-        map<Point, int> m1;
-        m1[p1]++; m1[p2]++; m1[p3]++; m1[p4]++;
-        if(m1.size() < 4) return 1;
-
     	// 비교를 일반화하기 위한 점 위치 변경
-        if (p1.x > p2.x) swap(p2, p1);
-        if (p3.x > p4.x) swap(p3, p4);
+        if (p1 > p2) swap(p2, p1);
+        if (p3 > p4) swap(p3, p4);
         
-        // return p3.x <= p2.x && p1.x <= p4.x; // 두 선분이 포개어져 있는지 확인
-        return (p1.x <= p4.x && p1.y <= p4.y) && (p3.x <= p2.x && p3.y <= p2.y);
+        return p3 <= p2 && p1 <= p4; // 두 선분이 포개어져 있는지 확인
     }
     
     return p1p2 <= 0 && p3p4 <= 0;
@@ -52,16 +39,16 @@ bool isIntersect(pair<Point, Point> l1, pair<Point, Point> l2) {
 
 int main() {
 	fastio;
-    Point p1, p2, p3, p4;
-    cin >> p1.x >> p1.y >> p2.x >> p2.y;
-    cin >> p3.x >> p3.y >> p4.x >> p4.y;
+
+    point p1, p2, p3, p4;
+
+    cin >> p1.first >> p1.second >> p2.first >> p2.second;
+    cin >> p3.first >> p3.second >> p4.first >> p4.second;
     
-    pair<Point, Point> l1 = {p1, p2};
-    pair<Point, Point> l2 = {p3, p4};
+    pair<point, point> l1 = {p1, p2};
+    pair<point, point> l2 = {p3, p4};
 
     int ans = isIntersect(l1, l2);
     
     cout << ans << '\n';
-
-    // cout <<ccw(p1, p2, p3)*ccw(p1, p2, p4) << " " << ccw(p3, p4, p1)*ccw(p3, p4, p2);
 }
