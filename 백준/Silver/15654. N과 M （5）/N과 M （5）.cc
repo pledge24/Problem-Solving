09c954f164a436a4f1cce9af61e9366391333d33
윤형석 <104922729+pledge24@users.noreply.github.com>
2024-10-14 00:36:39 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -4,51 +4,48 @@
 
 using namespace std;
 
-int N, M;
+int N, M; 
+vector<int> v1, curState;
 
-vector<int> v1, visited;
+// v1: 검색 공간을 담고있는 상태공간트리
+// curState: 현재 상태
 
-vector<int> ans;
-void dfs(int idx){
+void dfs(int size, int M){
 
-    if(ans.size() == M){
-        for(int elem: ans){
+    if(size > 0 && curState[size-1] == 0) return;
+
+    // 종료 조건
+    if(size == M){
+        for(int elem : curState){
             cout << elem << ' ';
         }
-        cout <<'\n';
+        cout << '\n';
 
         return;
     }
 
-    for(int i=0; i<v1.size(); i++){
-        if(!visited[i]){
-            visited[i] = true;
-            ans.push_back(v1[i]);
-            dfs(i+1);
-            visited[i] = false;
-            ans.pop_back();
-        }
+    for(int i = 0; i < N; i++){   
+        curState[size] = v1[i];
+
+        v1[i] = 0;
+        dfs(size+1, M);
+        v1[i] = curState[size];
     }
     
-        
-    
-
     return;
 }
 
 int main() {
 	fastio;
+    
     cin >> N >> M;
 
-    v1.resize(N);
-    visited.resize(N);
-
-    for(int i=0; i<N; i++){
+    v1.resize(N); curState.resize(M);
+    for(int i = 0; i < N; i++){
         cin >> v1[i];
     }
-    
-    sort(v1.begin(), v1.end());
-    
-    dfs(0);
 
+    sort(v1.begin(), v1.end());
+
+    dfs(0, M);
 }
