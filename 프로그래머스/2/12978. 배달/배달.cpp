8427052b096881c2bac36571d46124e762c95c6c
윤형석 <104922729+pledge24@users.noreply.github.com>
2024-10-21 02:59:23 +0900

    [level 2] Title: 배달, Time: 0.24 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -18,9 +18,15 @@ int solution(int V, vector<vector<int>> road, int K) {
     }
 
     vector<int> shortest_dist(V+1, INF);
-    
+    vector<bool> visited(V+1);
+
     // init
-    shortest_dist[1] = 0;
+    int start = 1;
+    shortest_dist[start] = 0;
+    for(pair<int, int> ed : graph[start]){
+        shortest_dist[ed.first] = min(shortest_dist[ed.first], shortest_dist[start] + ed.second);
+    }
+    visited[start] = true;
 
     // O(V)
     for(int i = 0; i < V-1; i++){
@@ -28,26 +34,29 @@ int solution(int V, vector<vector<int>> road, int K) {
         int minDist = INF;
         int selected_vtx = -1;
 
-        // 확정된 정점들로부터 뻗어나가는 모든 간선 체크 O(E)
+        // 다음 정점을 선택
+        minDist = INF;
         for(int vtx = 1; vtx <= V; vtx++){
-            // 아직 확정되지 않은 정점들은 패스
-            if(shortest_dist[vtx] == INF) continue;
-
-            for(pair<int, int> ed : graph[vtx]){
-                // 도착한 정점이 확정된 정점이면 패스
-                if(shortest_dist[ed.first] != INF) continue;
-                
-                if(shortest_dist[vtx] + ed.second < minDist){
-                    minDist = shortest_dist[vtx] + ed.second;
-                    selected_vtx = ed.first;
-                }
+            if(!visited[vtx] && shortest_dist[vtx] < minDist){
+                selected_vtx = vtx;
+                minDist = shortest_dist[vtx];
             }
         }
 
+        // 더 이상 선택할 수 있는 정점이 없는 경우 루프 탈출
+        if(selected_vtx == -1) break;
+
         // 선택된 정점을 확정셋에 추가
-        if(minDist != INF) shortest_dist[selected_vtx] = minDist;
-    }
+        shortest_dist[selected_vtx] = minDist;
+        visited[selected_vtx] = true;
         
+        // 선택된 정점을 거쳐가는 경로를 추가 및 최단 경로 갱신
+        for(pair<int, int> ed : graph[selected_vtx]){
+            shortest_dist[ed.first] = min(shortest_dist[ed.first], shortest_dist[selected_vtx] + ed.second);
+        }
+
+    }
+    
     int ans = 0;
     for(int i = 1; i <= V; i++){
         cout << shortest_dist[i] << '\n';
