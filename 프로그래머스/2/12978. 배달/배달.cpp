93f9a8466f300a30f28bdd3b73cd1543e2333e1b
윤형석 <104922729+pledge24@users.noreply.github.com>
2024-10-21 01:41:45 +0900

    [level 2] Title: 배달, Time: 0.50 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -2,40 +2,56 @@
 #include <vector>
 #include <bits/stdc++.h>
 
-#define MAXN 100'000'000
+#define INF 100'000'000
 
 using namespace std;
 
-int solution(int N, vector<vector<int>> road, int K) {
-    int answer = 1;
+int solution(int V, vector<vector<int>> road, int K) {   
     
-    vector<vector<int>> matrix;
-    vector<vector<int>> shortest_path;
+    vector<vector<pair<int, int>>> graph(V+1);
     
-    matrix.resize(N+1, vector<int>(N+1, MAXN));
-    shortest_path.resize(N+1, vector<int>(N+1));
-
     int src, dst, w;
     for(int i = 0; i < road.size(); i++){
         src = road[i][0], dst = road[i][1], w = road[i][2];
-        matrix[src][dst] = min(matrix[src][dst], w);
-        matrix[dst][src] = min(matrix[dst][src], w);
+        graph[src].push_back({dst, w});
+        graph[dst].push_back({src, w});
     }
 
-    shortest_path = matrix;
+    vector<int> shortest_dist(V+1, INF);
+    
+    // init
+    shortest_dist[1] = 0;
 
-    for(int k = 1; k <= N; k++){
+    // O(V)
+    for(int i = 0; i < V-1; i++){
 
-        for(int i = 1; i <= N; i++){
-            for(int j = 1; j <= N; j++){
-                if(i == j) continue;
-                shortest_path[i][j] = min(shortest_path[i][j], shortest_path[i][k] + shortest_path[k][j]);
+        int minDist = INF;
+        int selected_vtx = -1;
+
+        // 확정된 정점들로부터 뻗어나가는 모든 간선 체크 O(E)
+        for(int vtx = 1; vtx <= V; vtx++){
+            // 아직 확정되지 않은 정점들은 패스
+            if(shortest_dist[vtx] == INF) continue;
+
+            for(pair<int, int> ed : graph[vtx]){
+                // 도착한 정점이 확정된 정점이면 패스
+                if(shortest_dist[ed.first] != INF) continue;
+                
+                if(shortest_dist[vtx] + ed.second < minDist){
+                    minDist = shortest_dist[vtx] + ed.second;
+                    selected_vtx = ed.first;
+                }
             }
         }
+
+        // 선택된 정점을 확정셋에 추가
+        if(minDist != INF) shortest_dist[selected_vtx] = minDist;
     }
         
-    for(int i = 1; i <= N; i++){
-        if(shortest_path[1][i] <= K) answer++;
+    int ans = 0;
+    for(int i = 1; i <= V; i++){
+        cout << shortest_dist[i] << '\n';
+        if(shortest_dist[i] <= K) ans++;
     }
-    return answer;
+    return ans;
 }
