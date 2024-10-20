badc1406562a9127cd891e554b50f2b9bcca3e26
윤형석 <104922729+pledge24@users.noreply.github.com>
2024-10-20 17:53:10 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,74 +1,46 @@
-#include <bits/stdc++.h>
-
-#define fastio cin.tie(0)->sync_with_stdio(0)
-
+// http://boj.kr/2ecc5e3e8209416d84a6858c21448d81
+#include<bits/stdc++.h>
 using namespace std;
 
-struct cmp
-{
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b){
-        return a.second > b.second;
+#define X first
+#define Y second
+
+int v,e,st;
+
+// {비용, 정점 번호}
+vector<pair<int,int>> adj[20005];
+const int INF = 0x3f3f3f3f;
+int d[20005]; // 최단 거리 테이블
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> v >> e >> st;
+  fill(d,d+v+1,INF);
+  while(e--){
+    int u,v,w;
+    cin >> u >> v >> w;
+    adj[u].push_back({w,v});
+  }
+
+  priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+  d[st] = 0;
+  // 우선순위 큐에 (0, 시작점) 추가
+  pq.push({d[st],st});
+  while(!pq.empty()){
+    auto cur = pq.top(); pq.pop(); // {비용, 정점 번호}    
+    // 거리가 d에 있는 값과 다를 경우 넘어감
+    if(d[cur.Y] != cur.X) continue;
+    for(auto nxt : adj[cur.Y]){
+      if(d[nxt.Y] <= d[cur.Y]+nxt.X) continue;
+      // cur를 거쳐가는 것이 더 작은 값을 가질 경우
+      // d[nxt.Y]을 갱신하고 우선순위 큐에 (거리, nxt.Y)를 추가
+      d[nxt.Y] = d[cur.Y]+nxt.X;
+      pq.push({d[nxt.Y],nxt.Y});
     }
-};
+  }
 
-int V, E, K; 
-vector<vector<pair<int, int>>> graph; // first : node_no, second: weight
-
-vector<int> dijkstra_algorithm(int start_node_no){
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq; // first : node_no, second: distance
-    vector<int> shortest_dist(V+1, INT32_MAX);
-
-    // init
-    for(auto ed : graph[start_node_no]){
-        pq.push({ed.first, ed.second});
-        shortest_dist[ed.first] = ed.second;
-    }
-
-    while(!pq.empty()){
-        pair<int, int> cur_path = pq.top(); pq.pop();
-
-        if(cur_path.second != shortest_dist[cur_path.first]) continue;
-        for(pair<int, int> ed : graph[cur_path.first]){
-
-            int Next_node = ed.first;
-            int nDist = cur_path.second + ed.second;
-            
-            if(nDist < shortest_dist[Next_node]){
-                shortest_dist[ed.first] = nDist;
-                pq.push({ed.first, nDist});        
-            }
-        }
-
-    }
-
-    return shortest_dist;
-}
-
-int main() {
-	fastio;
-    cin >> V >> E >> K;
-
-    graph.resize(V+1);
-
-    int node1, node2, weight;
-    graph[K].push_back({K, 0});
-    for(int i = 0; i < E; i++){
-        cin >> node1 >> node2 >> weight;
-        graph[node1].push_back({node2, weight});
-    }  
-    
-    vector<int> ans;
-    ans = dijkstra_algorithm(K);
-
-    for(int i = 1; i<=V; i++){
-        if(ans[i] == INT32_MAX){
-            cout << "INF" << '\n';
-        }
-        else{
-            cout << ans[i] << '\n';
-        }
-        
-    }
-
+  for(int i = 1; i <= v; i++){
+    if(d[i] == INF) cout << "INF\n";
+    else cout << d[i] << "\n";
+  }
 }
