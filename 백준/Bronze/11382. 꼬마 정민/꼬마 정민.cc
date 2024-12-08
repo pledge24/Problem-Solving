087dca0c1d9cdac02f1f52b335a89b1f5fe8bbea
윤형석 <104922729+pledge24@users.noreply.github.com>
2024-12-08 23:06:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+int main() {
+	long long a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a + b + c << '\n';
+    
+}
