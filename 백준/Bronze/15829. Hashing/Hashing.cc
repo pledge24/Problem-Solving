85a317e3795e3b45fcb1f8b8a5fe63f9e7856fea
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-01-22 15:42:37 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -7,23 +7,23 @@ using namespace std;
 int main(void){
     fastio;
 
-    enum {R = 31, M = 1234567891};
-
+    long long R = 31;
+    long long M = 1234567891;
     int L; cin >> L;
 
-    int ans = 0;
+    long long ans = 0;
     for(int i = 0; i < L; i++){
         char c; cin >> c;
-        int num = (c - 'a') + 1;
+        long long num = (c - 'a') + 1;
 
         for(int j = 0; j < i; j++){
             num = (num * R) % M;
         }
 
-        ans += num;
+        ans = (ans + num) % M;
     }
 
     cout << ans << '\n';
-    
+
     return 0;
 }
