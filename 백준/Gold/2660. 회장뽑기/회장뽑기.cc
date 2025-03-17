a5f012aa9496b31be96788d70360a45f3c72b322
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-03-17 17:54:18 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int bfs(const Graph& graph, int start, int N){
+    int cnt = 0;
+    vector<int> score(N+1, -1);
+    queue<int> q;
+
+    score[start] = 0;
+    q.push(start);
+
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+
+        for(int adj : graph[cur]){
+            if(score[adj] == -1){
+                q.push(adj);
+                score[adj] = score[cur] + 1;
+                cnt = max(cnt, score[cur] + 1);
+            }
+        }
+    }
+
+    return cnt;
+}
+
+int main(void){
+    fastio;
+    int N; cin >> N;
+
+    Graph graph(N+1);
+    int A, B;
+    while(true){
+        cin >> A >> B;
+        if(make_pair(A, B) == make_pair(-1, -1))
+            break;
+
+        graph[A].push_back(B);
+        graph[B].push_back(A);
+    }
+
+    vector<pair<int, int>> scores;
+    for(int i = 1; i <= N; i++){
+        int cnt = bfs(graph, i, N);
+        scores.push_back(make_pair(cnt, i));
+    }
+
+    sort(scores.begin(), scores.end());
+
+    // int minScore = scores[0].first;
+    // int candidateN = count_if(scores.begin(), scores.end(), [minScore](auto elem){return elem.first == minScore;});
+    // cout << scores[0].first << ' ' << candidateN << '\n';
+    // for(int i = 0; i < candidateN; i++){
+    //     cout << scores[i].second << ' ';
+    // }
+    // cout << '\n';
+
+    int minScore = scores[0].first;
+    int candidateN = 0;
+    for(auto it : scores){
+        if(it.first > minScore)
+            break;
+        candidateN++;
+    }
+
+    cout << scores[0].first << ' ' << candidateN << '\n';
+    for(int i = 0; i < candidateN; i++){
+        cout << scores[i].second << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
