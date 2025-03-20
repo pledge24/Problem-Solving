d80948f69e3ef05f3d1ee91d4269b7b3269e0f1c
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-03-20 15:47:39 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+int main(void){
+    fastio;
+    
+    while(!cin.eof()){
+        string str; getline(cin, str);
+        cout << str << '\n';
+    }
+
+    return 0;
+}
