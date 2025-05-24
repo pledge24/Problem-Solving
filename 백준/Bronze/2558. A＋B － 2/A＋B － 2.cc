908d1cf6cf6d2ffa32bf2a42ae0396137ef0f130
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-05-25 02:31:49 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+int main (void){
+        fastio;
+        int A, B; cin >> A >> B;
+    
+        cout << A+B << '\n';
+    
+        return 0;
+}
