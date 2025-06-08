e8cd31a7191f4dcbd506ab33e0ca6960f4073086
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-06-08 15:45:47 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+using int64 = long long;
+
+int main(void){
+    fastio;
+    int64 N; cin >> N;
+
+    cout << N*4 << '\n';
+
+    return 0;
+}
