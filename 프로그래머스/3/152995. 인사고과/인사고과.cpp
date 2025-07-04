80342db41ecde21bbbfa71d743373008f3ef9bd5
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-07-04 16:02:19 +0900

    [level 3] Title: 인사고과, Time: 30.26 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -22,13 +22,17 @@ int solution(vector<vector<int>> scores) {
         // 통과가 되는 경우의 수
         // 1) maxFirst가 같음 -> 통과
         // 2) maxFirst가 다름 -> maxSecond를 이겨야함
+        // 3) [5, 1] -> [4, 5] -> [4, 3] 문제: 같은 first가 maxSecond가 되어 이전 maxSecond가 가려져서 탈락하는 문제
+        // 4) [7, 5] -> [6, 6] -> [6, 4] 문제: 같은 first가 maxSecond가 되어 이전 maxSecond가 가려져서 통과되는 문제
+        // => 결국 second에 대해선 오름차순 정렬 필요(같은 first에 가려지는 문제 해결)
+        // 확장 일반화 고려: n개의 점수가 있었다면?
         if(scores[i][0] == maxFirst || scores[i][1] >= maxSecond){
             incentiveList.push_back(scores[i]);
             maxSecond = max(maxSecond, scores[i][1]);
             // cout << scores[i][0] << " " << scores[i][1] << '\n';
         }
     }
-    
+       
     int answer = 0;
     int wanho_score = wanho[0] + wanho[1];
     if(find(incentiveList.begin(), incentiveList.end(), wanho) == incentiveList.end()){
