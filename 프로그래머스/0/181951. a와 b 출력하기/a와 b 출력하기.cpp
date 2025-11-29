d2292c540a6b9042f094814839429ffe7e2a1082
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-11-30 01:11:09 +0900

    [level 0] Title: a와 b 출력하기, Time: 2.91 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}
