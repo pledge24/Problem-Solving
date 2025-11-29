0e2337d0f0ca0d9d132d72c961d71ab2ade87da3
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-11-30 01:14:24 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 2.89 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for(char& c : str){
+        c = isupper(c) ? tolower(c) : toupper(c);
+    }
+    
+    cout << str << '\n';
+    
+    return 0;
+}
