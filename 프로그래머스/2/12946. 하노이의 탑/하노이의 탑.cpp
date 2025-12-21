2530896961db022fa8f3ab91876a140ee136f612
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-12-21 12:19:15 +0900

    [level 2] Title: 하노이의 탑, Time: 25.37 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -1,26 +1,34 @@
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-// n: 원판 개수, from: 시작 기둥, to: 목적 기둥, bypass: 거쳐가는 기둥
-void hanoi(int n, int from, int to, int bypass, vector<vector<int>>& answer) {
-    if (n == 1) {
-        answer.push_back({from, to});
+const int LEFT = 0;
+const int CENTER = 1;
+const int RIGHT = 2;
+
+void DnC(int n, int src, int dst, int bypass, vector<vector<int>>& answer)
+{
+    if(n == 1)
+    {
+        answer.push_back({src+1, dst+1});
         return;
     }
-
-    // 1. 가장 큰 원판을 제외한 n-1개를 bypass 기둥으로 옮김
-    hanoi(n - 1, from, bypass, to, answer);
     
-    // 2. 가장 큰 원판을 목적지로 옮김
-    answer.push_back({from, to});
+    // 1. n-1 길이의 탑을 byPass에 옮긴다.
+    DnC(n-1, src, bypass, dst, answer);
+        
+    // 2. 가장 아래에 있는 원판을 목적지에 옮긴다.
+    DnC(1, src, dst, bypass, answer);
     
-    // 3. bypass에 있던 n-1개를 다시 목적지로 옮김
-    hanoi(n - 1, bypass, to, from, answer);
+    // 3. n-1 길이의 탑을 목적지에 옮긴다.
+    DnC(n-1, bypass, dst, src, answer);
 }
 
 vector<vector<int>> solution(int n) {
     vector<vector<int>> answer;
-    hanoi(n, 1, 3, 2, answer);
+    
+    // n 길이의 탑 -> 가장 아래에 있는 원판 + n-1 길이의 탑
+    DnC(n, LEFT, RIGHT, CENTER, answer);
+    
     return answer;
 }
