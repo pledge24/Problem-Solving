d0815e1c017af2fb3e75f2578bcae2a05fd53515
윤형석 <104922729+pledge24@users.noreply.github.com>
2025-12-23 12:32:55 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    int len = my_string.length();
+    
+    answer = my_string.substr(len-n);
+    return answer;
+}
