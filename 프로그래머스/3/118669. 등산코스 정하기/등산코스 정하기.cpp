d31f8abc3651db25582456d44852ef11a09b4ccd
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-01-13 16:06:53 +0900

    [level 3] Title: 등산코스 정하기, Time: 101.93 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.cpp
@@ -1,135 +1,99 @@
 #include <bits/stdc++.h>
 
+#define INF 100'000'000
+
 using namespace std;
-using EdgeData = pair<int, pair<int, int>>;     // <cost, <vtx1, vtx2>>
-using AdjList = vector<pair<int, int>>;         // <dst, cost>
-using Tree = vector<AdjList>;    
 
-vector<int> parent;
-const int MAX_W = 10'000'000;
+using EdgeData = pair<int, int>; // <cost, vtxNo>
+using PathData = pair<int, int>; // <intensity, vtxNo>
+using Graph = vector<vector<EdgeData>>;
 
-// union-find 알고리즘(경로 압축 + union by rank포함)
-int find(int x){
-    if(parent[x] < 0) return x;
-    return parent[x] = find(parent[x]);
-}
-
-bool is_diff_group(int u, int v){
-    u = find(u); 
-    v = find(v);
-
-    if(u == v) return false;
-
-    if(parent[u] == parent[v]) parent[u]--;
-
-    if(parent[u] < parent[v]) parent[v] = u;
-    else parent[u] = v;
-
-    return true;
-}
-
-pair<int, int> Bfs(Tree& mst, int gateNo, vector<bool>& isSummits)
+PathData dijkstra(Graph& graph, vector<int>& gates, vector<int>& summits)
 {
-    int N = mst.size();
-    vector<bool> visited(N,false);
-    queue<pair<int, int>> q;    // <MaxIntensity, curVtxNo>
-    {
-        q.push(make_pair(0, gateNo));
-        visited[gateNo] = true;
-    }
+    int N = graph.size();
+    priority_queue<PathData, vector<PathData>, greater<PathData>> pq; // 후보들이 들어감
+    vector<int> minIntensity(N, INF);
     
-    pair<int, int> res = make_pair(MAX_W+1, -1); // intensity, vtxNo
-    while(!q.empty())
+    vector<bool> isGate(N, false);
+    vector<bool> isSummit(N, false);
+    
+    // Init.
     {
-        pair<int, int> p = q.front(); q.pop();
-        int maxIntensity = p.first;
-        int vtxNo = p.second;
-        
-        AdjList& adj = mst[vtxNo];
-        for(auto& p : adj)
+        for(const int& gateNo : gates)
         {
-            int nextVtxNo = p.first;
-            int intensity = p.second;
-            
-            if(visited[nextVtxNo])
-                continue;
-            
-            visited[nextVtxNo] = true;
-            pair<int, int> next = make_pair(max(intensity, maxIntensity), nextVtxNo);
-            
-            if(isSummits[nextVtxNo])
-                res = min(res, next);
-            else
-                q.push(next);
+            pq.push(make_pair(0, gateNo));
+            isGate[gateNo] = true;
+        }
+
+        for(const int& summit : summits)
+        {
+            isSummit[summit] = true;
         }
     }
     
+    // Process
+    while(!pq.empty())
+    {
+        PathData pd = pq.top(); pq.pop();
+        int intensity = pd.first;
+        int vtxNo = pd.second;
+        
+        if(intensity >= minIntensity[vtxNo])
+            continue;
+        
+        minIntensity[vtxNo] = intensity;
+        
+        if(isSummit[vtxNo])
+            continue;
+        
+        for(EdgeData& ed : graph[vtxNo])
+        {
+            int pathIntensity = max(intensity, ed.first);
+            int nextVtxNo = ed.second;
+
+            if(isGate[nextVtxNo])
+                continue;
+            
+            pq.push(make_pair(pathIntensity, nextVtxNo));
+        }
+    }
     
-    return res;
+    // for(int elem : minIntensity)
+    // {
+    //     cout << elem << ' ';
+    // }
+    // cout << '\n';
+    
+    PathData minPd = make_pair(INF, -1);
+    for(int summ : summits)
+    {
+        int inten = minIntensity[summ];
+        PathData pd = make_pair(inten, summ);
+        
+        minPd = min(minPd, pd);
+    }
+    
+    
+    return minPd;
 }
 
-// MST를 크루스칼 알고리즘으로 만들면 끝난다.
 vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
     vector<int> answer;
     
-    priority_queue<EdgeData, vector<EdgeData>, greater<EdgeData>> pq;
-    for(vector<int>& p : paths)
+    Graph graph(n+1);
+    for(const vector<int>& path : paths)
     {
-        int i = p[0];
-        int j = p[1];
-        int w = p[2];
+        int vtx1 = path[0];
+        int vtx2 = path[1];
+        int w = path[2];
         
-        EdgeData data = make_pair(w, make_pair(i, j));
-        pq.push(std::move(data));
+        graph[vtx1].push_back(make_pair(w, vtx2));
+        graph[vtx2].push_back(make_pair(w, vtx1));
     }
     
-    // Make MST
-    Tree mst(n+1);
-    parent.resize(n+1, -1);
-    int cnt = 0;
-    while(!pq.empty())
-    {
-        EdgeData ed = pq.top(); pq.pop();
-        int cost = ed.first;
-        pair<int, int> endPoint = ed.second;
-        int vtx1 = endPoint.first;
-        int vtx2 = endPoint.second;
-        
-        if(!is_diff_group(vtx1, vtx2))
-            continue;
-              
-        // Add Adj
-        mst[vtx1].push_back(make_pair(vtx2, cost));
-        mst[vtx2].push_back(make_pair(vtx1, cost));
-        
-        cnt++;
-        
-        if(cnt == n-1)
-            break;
-    }
+    pair<int, int> p = dijkstra(graph, gates, summits);
     
-    vector<bool> isSummits(n, false);
-    for(const int& elem : summits)
-    {
-        isSummits[elem] = true;
-    }
-    
-    bool first = true;
-    pair<int, int> res; // intensity, vtxNo
-    for(const int& gate : gates)
-    {
-        pair<int, int> p = Bfs(mst, gate, isSummits);
-        if(first)
-        {
-            first = false;
-            res = p;
-            continue;
-        }
-        
-        res = min(res, p);
-    }
-    
-    answer = {res.second, res.first};
+    answer = {p.second, p.first};
     
     return answer;
 }
