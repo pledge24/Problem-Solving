f2ff311bdd358c07aa5fb24500b761c95c34f0b8
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-02-03 19:11:59 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    for(int num : num_list)
+    {
+        while(num > 1)
+        {
+            num /= 2;
+            answer++;
+        }
+    }
+    return answer;
+}
