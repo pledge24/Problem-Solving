46eb8c1ec184475cbf60786503b484d30d27365b
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-02-05 23:41:34 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.47 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -2,21 +2,6 @@
 
 using namespace std;
 
-// '방금그곡' 서비스.
-// - 고객이 기억한 멜로디를 소스로 사용해 음악을 찾는다.
-// - 한 음악을 반복 재생 -> 기억한 멜로디의 시작이 음악의 시작 부분이 아닐 수 있음.
-// - 한 음악을 중간에 끊음 -> 찾은 곡에도 있는 멜로디지만, 들은 곡은 아닐 수 있음.
-// So, 기억한 멜로디를 1) 재생 시간과 2) 제공된 악보를 직접 보며 비교하려고 한다.
-
-// 서비스는 1) 구간 ([재생 시작, 끝남]), 2) 제목, 3) 악보 제공
-// 각 음은 1분에 1개씩 재생됨. 
-// - 항상 처음부터 재생
-// - 재생 길이 > 음악 길이라면, 반복 재생으로 간주
-// - 재생 길이 < 음악 길이라면, 처음부터 재생 시간만큼 재생
-// - 음악은 00:00을 초과하지 않는다.(HH:MM)
-// - 후보가 여러개라면 "라디오에서 재생된 시간이 제일 긴 음악 제목"을 반환.
-// - 재생된 시간도 같다면, 먼저 입력된 음악 제목을 반환
-
 void Split(string& s, vector<string>& splitted, string delimeter)
 {
     int pos = 0;
@@ -38,6 +23,28 @@ void Split(string& s, vector<string>& splitted, string delimeter)
     }
 }
 
+string Normalize(string& s)
+{
+    string res = "";
+    int len = s.length();
+    
+    for(int i = 0; i < len; i++)
+    {
+        char c = s[i];
+        if(i + 1 < len && s[i+1] == '#')
+        {
+            res += tolower(c);
+            i++;
+        }
+        else
+        {
+            res += c;
+        }
+    }
+    
+    return res;
+}
+
 string solution(string m, vector<string> musicinfos) {
     string answer = "";
     
@@ -47,17 +54,7 @@ string solution(string m, vector<string> musicinfos) {
         int playTime = 0;
     };
     
-    string mTemp = "";
-    for(int i = 0; i < m.length(); i++)
-    {
-        char c = m[i];
-        if(isalpha(c) == false)
-            mTemp[mTemp.length()-1]++;
-        else
-            mTemp += char((c - 'A') * 2 + 'A');
-    }
-    
-    std::swap(mTemp, m);
+    m = Normalize(m);
     cout << m << '\n';
     
     vector<musicData> candidate;
@@ -71,57 +68,27 @@ string solution(string m, vector<string> musicinfos) {
         string& startTime = splitted[0];
         string& endTime = splitted[1];
         string& title = splitted[2];
-        string& note = splitted[3];
+        string note = Normalize(splitted[3]);
         
         // Get PlayTime
         int playTime = 0;
         {
-            vector<string> sTime;
-            vector<string> eTime;
-            Split(startTime, sTime, ":");
-            Split(endTime, eTime, ":");
-
-            int sHH = stoi(sTime[0]);
-            int sMM = stoi(sTime[1]);
-            int eHH = stoi(eTime[0]);
-            int eMM = stoi(eTime[1]);
+            // Tips: 크기가 일정한 경우, 그냥 substr만 써도 충분. 코드를 간결화 효과 챙기자
+            int sHH = stoi(startTime.substr(0, 2));
+            int sMM = stoi(startTime.substr(3, 2));
+            int eHH = stoi(endTime.substr(0, 2));
+            int eMM = stoi(endTime.substr(3, 2));
             
             playTime = (eHH - sHH)*60 + eMM - sMM;
         }
         
-        string noteX = "";
-        for(int i = 0; i < note.length(); i++)
+        string played = "";
+        for(int i = 0; i < playTime; i++)
         {
-            char c = note[i];
-            if(isalpha(c) == false)
-                noteX[noteX.length()-1]++;
-            else
-                noteX += char((c - 'A') * 2 + 'A');
+            played += note[i % note.length()];
         }
         
-        string temp = "";
-        int len = 0;
-        int curIdx = 0;
-        while(len < playTime)
-        {
-            temp += char((note[curIdx] - 'A') * 2 + 'A');
-            
-            // Check
-            if(isalpha(note.substr(curIdx, 2).back()) == false)
-            {
-                temp[temp.length()-1]++;
-                curIdx = (curIdx + 1) % note.length();
-            }
-            
-            // Update
-            len++;
-            curIdx = (curIdx + 1) % note.length();
-        }
-        
-        // cout << "noteX: " << noteX << '\n';
-        // cout << "temp: " << temp << '\n';
-        
-        if(temp.find(m) != string::npos)
+        if(played.find(m) != string::npos)
         {
             musicData md = {title, playTime};
             candidate.push_back(md); 
