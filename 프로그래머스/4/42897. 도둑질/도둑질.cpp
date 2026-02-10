59de5c1cfe6031de2777b632eb0dc1096dd9f3a3
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-02-11 07:56:36 +0900

    [level 4] Title: 도둑질, Time: 7.28 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -6,48 +6,24 @@ int solution(vector<int> money) {
     int answer = 0;
     int moneyN = money.size();
     
-    if(moneyN == 3)
-        return max(money[0] + money[2], money[1]);
+    vector<int> dp1(moneyN);    // 첫번째 집 털
+    vector<int> dp2(moneyN);    // 첫번째 집 안털
     
-    // ===n이 4 이상인 경우===
-    
-    // 첫번째 집 털
+    dp1[0] = money[0];
+    dp1[1] = max(money[0], money[1]);
+    for(int i = 2; i < moneyN-1; i++)
     {
-        vector<int> dp1(moneyN);
-        vector<int> dp2(moneyN);
-        dp1[2] = money[0] + money[2];
-        dp2[2] = money[0];
-        for(int i = 3; i < moneyN; i++)
-        {
-            if(i == moneyN-1)
-            {
-                dp1[i] = dp1[i-1];
-                dp2[i] = dp2[i-1];
-                continue;
-            }
-            
-            dp1[i] = dp2[i-1] + money[i];
-            dp2[i] = max(dp1[i-1], dp2[i-1]);
-        }
-        
-        answer = max(dp1[moneyN-1], dp2[moneyN-1]);
+        dp1[i] = max(dp1[i-2] + money[i], dp1[i-1]);
     }
     
-    
-    // 첫번째 집 안털
+    dp2[0] = 0;
+    dp2[1] = money[1];
+    for(int i = 2; i < moneyN; i++)
     {
-        vector<int> dp1(moneyN);
-        vector<int> dp2(moneyN);
-        dp1[1] = money[1];
-        dp2[1] = 0;
-        for(int i = 2; i < moneyN; i++)
-        {
-            dp1[i] = dp2[i-1] + money[i];
-            dp2[i] = max(dp1[i-1], dp2[i-1]);
-        }
-        
-        answer = max({answer, dp1[moneyN-1], dp2[moneyN-1]});
+        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1]);
     }
+    
+    answer = max(dp1[moneyN-2], dp2[moneyN-1]);
     
     return answer;
 }
