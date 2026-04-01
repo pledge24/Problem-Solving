01aee4f4f80ee9e0da6216a7f70110e364c14f82
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-04-02 02:07:52 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+int main (void){
+        fastio;
+        string str; cin >> str;
+    
+        cout << str.length() << '\n';
+    
+        return 0;
+}
