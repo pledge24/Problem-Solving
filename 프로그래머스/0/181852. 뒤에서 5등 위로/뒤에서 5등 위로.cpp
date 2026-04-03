6c5d66e8c13cd21c5be00a4e48568dfcc1a08240
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-04-03 20:22:46 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    
+    std::sort(num_list.begin(), num_list.end());
+    
+    answer.assign(num_list.begin()+5, num_list.end());
+    return answer;
+}
