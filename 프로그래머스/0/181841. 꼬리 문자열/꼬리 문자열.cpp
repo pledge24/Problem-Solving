8f754855c23a96defadc80e852125d4eb20fcee1
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-04-15 16:15:17 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.02 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(vector<string> str_list, string ex) {
+    string answer = "";
+    
+    for(const string& str : str_list)
+    {
+        if(str.find(ex) == string::npos)
+            answer += str;
+    }
+    
+    return answer;
+}
