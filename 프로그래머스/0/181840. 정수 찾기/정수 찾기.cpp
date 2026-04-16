68677456d6beb5b281479e81bc743e3b97acdb41
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-04-16 16:26:14 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    int answer = std::find(num_list.begin(), num_list.end(), n) != num_list.end();
+    return answer;
+}
