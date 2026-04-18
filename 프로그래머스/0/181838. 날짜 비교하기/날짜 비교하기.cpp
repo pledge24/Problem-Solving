33aaf75922cb72f2822fab0ba2bc649af64a4717
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-04-18 19:34:48 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {
+    int answer = 0;
+    
+    answer = date1 < date2;
+    
+    return answer;
+}
