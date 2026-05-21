9ad6fcbfc0948406cc5a700df2d749b070360d37
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-05-21 12:24:58 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    
+    while(order > 0)
+    {
+        int digit = order % 10;
+        if(digit > 0 && digit % 3 == 0)
+        {
+            answer++;
+        }
+        
+        order /= 10;
+    }
+    return answer;
+}
