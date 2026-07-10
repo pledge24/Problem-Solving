30a557390630bda6568d4faaf9ec34d1e017ccf7
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-07-10 14:08:56 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    std::reverse(my_string.begin(), my_string.end());
+    
+    answer = my_string;
+    
+    return answer;
+}
