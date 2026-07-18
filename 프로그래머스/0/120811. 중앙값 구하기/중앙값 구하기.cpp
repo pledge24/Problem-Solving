38c642bd6128361fff0fa9c28111724f078f0bf9
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-07-18 19:27:08 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    
+    std::sort(array.begin(), array.end());
+    
+    answer = array[array.size() / 2];
+    
+    return answer;
+}
