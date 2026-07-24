a0acb476fdb56d795f98ed39b735c915fb53728f
윤형석 <104922729+pledge24@users.noreply.github.com>
2026-07-24 13:28:28 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    while(b % 2 == 0)
+        b /= 2;
+    
+    while(b % 5 == 0)
+        b /= 5;
+    
+    answer = (a % b > 0) + 1;
+    
+    return answer;
+}
